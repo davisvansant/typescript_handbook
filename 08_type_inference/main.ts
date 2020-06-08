6acf18223558a9bb5696a2ba61baabb687246436
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-06-08 13:09:31 -0700

    initial type inference

---
+++ 08_type_inference/main.ts
@@ -0,0 +1,8 @@
+let x = 3;
+console.log(typeof(x));
+
+let another_x = [0, 1, null, "hi"];
+console.log(typeof(another_x[0]));
+console.log(typeof(another_x[1]));
+console.log(typeof(another_x[2]));
+console.log(typeof(another_x[3]));
