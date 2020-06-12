fa6d257a3c1e2a3a040200343b9d79ba8f58f5b1
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-06-12 10:13:43 -0700

    14 - namespaced validators

--- 14_namespaces/main.ts
+++ 14_namespaces/main.ts
@@ -1,27 +1,29 @@
-interface StringValidator {
-  isAcceptable(s: string): boolean;
-}
-
-let lettersRegexp = /^[A-Za-z]+S/;
-let numbersRegexp = /^[0-9]+S/;
-
-class LettersOnlyValidator implements StringValidator {
-  isAcceptable(s: string) {
-    return lettersRegexp.test(s);
+namespace Validation {
+  export interface StringValidator {
+    isAcceptable(s: string): boolean;
   }
-}
 
-class ZipCodeValidator implements StringValidator {
-  isAcceptable(s: string) {
-    return s.length === 5 && numbersRegexp.test(s);
+const lettersRegexp = /^[A-Za-z]+S/;
+const numbersRegexp = /^[0-9]+S/;
+
+export class LettersOnlyValidator implements StringValidator {
+    isAcceptable(s: string) {
+      return lettersRegexp.test(s);
+    }
+  }
+
+export class ZipCodeValidator implements StringValidator {
+    isAcceptable(s: string) {
+      return s.length === 5 && numbersRegexp.test(s);
+    }
   }
 }
 
 let strings = [ "Hello", "98052", "101" ];
-let validators: { [s: string]: StringValidator; } = {};
+let validators: { [s: string]: Validation.StringValidator; } = {};
 
-validators["ZIP code"] = new ZipCodeValidator();
-validators["Letters only"] = new LettersOnlyValidator();
+validators["ZIP code"] = new Validation.ZipCodeValidator();
+validators["Letters only"] = new Validation.LettersOnlyValidator();
 
 for (let s of strings) {
   for (let name in validators) {
