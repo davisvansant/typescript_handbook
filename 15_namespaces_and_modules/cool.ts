de819ff2353b067d0ef6464bd99153c76defc9e6
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-06-15 12:08:36 -0700

    initial namespaces and modules

---
+++ 15_namespaces_and_modules/cool.ts
@@ -0,0 +1,4 @@
+export function do_cool_stuff(): String {
+    let some_string: String = "awesome";
+    return some_string;
+  }
---
+++ 15_namespaces_and_modules/main.ts
@@ -0,0 +1,8 @@
+import * as droopy from "./cool";
+import * as shapes from "./shapes";
+
+let awesome_function = droopy.do_cool_stuff();
+let awesome_shape = new shapes.Triangle();
+
+console.log(awesome_function);
+console.log(awesome_shape);
